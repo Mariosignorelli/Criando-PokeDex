667a3041332cdeff21b706303d3dfdb4432f41a7
Mario Signorelligit config --global user.email signorelii2308@gmail.com <signorelii2308@gmail.com>
2023-04-07 16:57:05 -0300

    add pokemon via api

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,35 @@ const limit = 10;
 const url = ` https://pokeapi.co/api/v2/pokemon?offset=${offset}limit=${limit}`;
 
 
+function covertPokemonToLi(pokemon){
+    return `
+    <li class="pokemon">
+            <span class="number">#001</span>
+            <pan class="name">${pokemon.name}</span>
+                <div class="delits">
+                    <ol class="types">
+                        <li class="type">Grass</li>
+                        <li class="type">paison</li>
+                    </ol>
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                </div> 
+        </li>        
+
+
+    `
+}
+const pokemonList = document.getElementById('pokemonList');
+
+
 fetch(url)
 .then( (response) => response.json())
-.then((responseBody) => console.log(responseBody))
-.catch((error) => comsole.log(error))
+.then((jsonBody) => jsonBody.results)
+.then((pokemons) =>{
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons [i];
+        pokemonList.innerHTML += covertPokemonToLi(pokemon)
+   
+        
+    }
+})
+.catch((error) => console.log(error))
